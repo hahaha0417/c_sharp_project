b75d3ef92ec8b3176a5cf967684eba06cabdc9ae
hahaha <hahaha0417@hotmail.com>
2025-08-24 16:49:08 +0800

    fix

---
+++ batch_file/demo/git clone.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# pwd
+
+# --------------------------------------------
+# demo
+# --------------------------------------------
+cd ../..
+mkdir demo
+
+
+git clone https://github.com/hahaha0417/c_sharp_demo.git demo
+
+# --------------------------------------------
+# hahahalib
+# --------------------------------------------
+cd demo
+mkdir hahahalib
+
+
+git clone https://github.com/hahaha0417/c_sharp_hahahalib.git hahahalib
