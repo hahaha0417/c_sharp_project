a123079a3b971c895acc3850e27c30a81e44315a
hahaha <hahaha0417@hotmail.com>
2025-08-24 18:44:03 +0800

    fix

---
+++ batch_file/demo/git clone - demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# pwd
+
+# --------------------------------------------
+# demo
+# --------------------------------------------
+cd ../..
+mkdir demo
+
+
+git clone https://github.com/hahaha0417/c_sharp_demo.git demo
---
+++ batch_file/demo/git clone - hahahalib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# pwd
+
+# --------------------------------------------
+# demo
+# --------------------------------------------
+cd ../..
+mkdir demo
+
+
+
+# --------------------------------------------
+# hahahalib
+# --------------------------------------------
+cd demo
+mkdir hahahalib
+
+
+git clone https://github.com/hahaha0417/c_sharp_hahahalib.git hahahalib
